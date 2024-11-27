5179f7710dbdef5ef34d956258a96637d0868575
TTaiJin <taeparis980110@gmail.com>
2024-11-27 23:02:03 +0900

    f3 B

--- src/test/java/com/ll/standard/util/UtilTest.java
+++ src/test/java/com/ll/standard/util/UtilTest.java
@@ -1,6 +1,5 @@
 package com.ll.standard.util;
 
-import jdk.jshell.execution.Util;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -11,12 +10,41 @@ public class UtilTest {
     @DisplayName("파일을 생성할 수 있다.")
     public void t1() {
         // given
-        String filePath = "test.txt";
+        String filePath = "temp/test.txt";
 
         // when
-        Util.file.touch(path);
+        Util.file.touch(filePath);
 
         // then
         assertThat(Util.file.exists(filePath)).isTrue();
+        Util.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일의 내용을 수정할 수 있고, 읽을 수 있다.")
+    public void t2() {
+        // given
+        String filePath = "temp/test.txt";
+
+        // when
+        Util.file.set(filePath, "내용");
+
+        // then
+        assertThat(Util.file.get(filePath, "")).isEqualTo("내용");
+        Util.file.delete(filePath);
+    }
+
+    @Test
+    @DisplayName("파일을 삭제할 수 있다.")
+    public void t3() {
+        // given
+        String filePath = "temp/test.txt";
+
+        // when
+        Util.file.set(filePath, "내용");
+        Util.file.delete(filePath);
+
+        // then
+        assertThat(Util.file.nonExists(filePath)).isTrue();
     }
 }
